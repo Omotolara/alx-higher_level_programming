5c5c43aa683dd7bee1471c3c0e39bef3a5e2e44a
omotolara <ibkomotola4real@gmail.com>
2023-08-16 15:22:10 -0700

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    for element in my_list:
+        if element == search:
+            new_list = [replace]
+        else:
+            new_list = [element]
+        return new_list
