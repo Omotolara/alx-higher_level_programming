55acb8bcea5a20eae354d956de794ce542da7b57
omotolara <ibkomotola4real@gmail.com>
2023-08-15 17:25:52 -0700

    Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for r in matrix:
         for c in r:
-            print("{:d}".format(num), end="$\n" if num == row[-1] else " ")
-        print("$")
+            print("{:d}".format(c), end="$\n" if c == r[-1] else " ")
+        print("")
