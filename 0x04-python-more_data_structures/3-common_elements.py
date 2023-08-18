07defa9c188d3adf08a113d2d83f2bbbf5fc91f6
omotolara <ibkomotola4real@gmail.com>
2023-08-18 07:38:16 -0700

    common elements in a set

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    comm_elements = set()
+    for element in set_1:
+        if element in set_2:
+            comm_elements.add(element)
+    return comm_elements
