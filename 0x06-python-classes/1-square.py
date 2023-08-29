474ee39fd04607f039794423226f57015c8a2915
omotolara <ibkomotola4real@gmail.com>
2023-08-29 12:28:38 -0700

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square with size"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """Instantiation with size.
+
+        Args:
+        size: the size of the square.
+        """
+        self.__size = size
