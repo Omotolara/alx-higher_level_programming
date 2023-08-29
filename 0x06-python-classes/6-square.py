a13cdf8f7347e288d60595718ad6495790d1828f
omotolara <ibkomotola4real@gmail.com>
2023-08-29 14:05:31 -0700

    coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,8 +11,8 @@ class Square:
         Args:
         size: the size of the square.
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
