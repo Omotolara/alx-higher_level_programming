7b651681ff3fc1267ee5fbc939ef8fe95922c418
omotolara <ibkomotola4real@gmail.com>
2023-09-04 09:05:35 -0700

    string repr of a rectangle

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -59,6 +59,6 @@ class Rectangle:
             return ""
         return ("\n".join(["#" * self.__width] * self.__height))
 
-    def __repr(self):
+    def __repr__(self):
         """Returns a string representation of the rectangle"""
         return ("Rectangle({}, {})".format(self.__width, self.__height))
