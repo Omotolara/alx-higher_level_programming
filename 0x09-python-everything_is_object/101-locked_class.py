67adb49d22f30bf7b62110df8e38e7dac6f249fd
omotolara <ibkomotola4real@gmail.com>
2023-09-05 09:39:13 -0700

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,8 +6,4 @@ class LockedClass:
     """Prevents the user from dynamically creating new instance attributes
     Except if the new instance attribute is called first_name.
     """
-    def __fixattr__(self, name, value):
-        """Fixes the new instance attribute to only be first_name"""
-        if name == 'first_name':
-            super().__fixattr__(name, value)
-        raise AttributeError
+    __slots__ = ["first_name"]
